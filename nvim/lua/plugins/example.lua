581e3199764c69e5f83714b9e8abe36f886c86d0
Vladislav Vlasov <axymria@gmail.com>
2023-11-16 10:10:37 +0400

    wip

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -85,6 +85,21 @@ return {
     end,
   },
 
+ --  {
+ --    "nvim-neo-tree/neo-tree.nvim",
+ --    opts = {
+ --            folder_closed = "",
+ --      icon = {
+ --            folder_open = "",
+ --            folder_empty = "󰜌",
+ --            -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+ --            -- then these will never be used.
+ --            default = "*",
+ --            highlight = "NeoTreeFileIcon"
+ --      },
+ --    }
+ --  },
+
   {
     "simrat39/rust-tools.nvim",
     opts = {}
