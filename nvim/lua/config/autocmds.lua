7c89c81f63f9bbbc5463f3785f992ec18570fad4
Vladislav Vlasov <axymria@gmail.com>
2024-05-18 00:56:39 +0400

    wip

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("my_lazyvim_" .. name, { clear = true })
+end
+
+local is_stable_version = true
+
+if is_stable_version then
+  -- Set filetype for .hurl files
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    group = augroup("env_filetype"),
+    pattern = { "*.hurl" },
+    callback = function()
+      vim.opt_local.filetype = "sh"
+    end,
+  })
+end
