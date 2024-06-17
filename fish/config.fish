a229af2a0f3c007db64513747703b37da03a8032
Vladislav Vlasov <axymria@gmail.com>
2024-06-17 21:37:34 +0400

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 export PATH=$HOME/bin:/usr/local/bin:/opt/homebrew/bin:~/.cargo/bin:/usr/bin:/bin:$PATH
+export BAT_THEME="Catppuccin Mocha"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
