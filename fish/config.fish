b3dbb1211af3262916074dd41ce2e4dbad74d798
Vladislav Vlasov <axymria@gmail.com>
2024-06-17 21:03:14 +0400

    add fish shell config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,8 @@
+export PATH=$HOME/bin:/usr/local/bin:/opt/homebrew/bin:~/.cargo/bin:/usr/bin:/bin:$PATH
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    zoxide init fish | source
+    fzf --fish | source
 end
