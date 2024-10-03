f590edd884b50c3c1b98ea0497af03ee2b04bdd6
Vladislav Vlasov <vladislavvlas7@gmail.com>
2024-10-03 22:20:51 +0400

    wip

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,3 +1,11 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
 }
