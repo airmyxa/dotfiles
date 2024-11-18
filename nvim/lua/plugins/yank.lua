67b24d703fc9c6ff8c20fa9eac9fa167e87a314c
Vladislav Vlasov <vladislavvlas7@gmail.com>
2024-11-18 19:48:05 +0400

    add yank plugin

---
+++ nvim/lua/plugins/yank.lua
@@ -0,0 +1,31 @@
+return {
+
+  {
+    "gbprod/yanky.nvim",
+    dependencies = {
+      { "kkharji/sqlite.lua" },
+    },
+    opts = {
+      ring = { storage = "sqlite" },
+      highlight = {
+        on_put = false,
+        on_yank = ,
+        timer = 50,
+      },
+    },
+    keys = {
+      {
+        "<leader>p",
+        function()
+          require("telescope").extensions.yank_history.yank_history({})
+        end,
+        desc = "Open Yank History",
+      },
+      { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+      { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+      { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+      { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+      { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+    },
+  },
+}
