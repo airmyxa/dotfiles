03d7b204397347138192429bc65738b0041e2306
Vladislav Vlasov <axymria@gmail.com>
2025-10-14 21:59:57 +0400

    wip

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -87,7 +87,6 @@ return {
                 rangeVariableTypes = true,
               },
               analyses = {
-                fieldalignment = true,
                 nilness = true,
                 unusedparams = true,
                 unusedwrite = true,
